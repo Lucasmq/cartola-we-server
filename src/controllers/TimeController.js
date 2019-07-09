69c28d7310e41018144efa58f8edc7131ab6eb5c
Lucas Queiroz <lucasmedeirosq@gmail.com>
2019-07-09 00:48:14 -0300

    Recebendo as fotos dos jogadores e os times

--- src/controllers/TimeController.js
+++ src/controllers/TimeController.js
@@ -53,8 +53,8 @@ module.exports = {
                     time['atletas'][i].nome = undefined;
                     time['atletas'][i].apelido = encurtaNome(time['atletas'][i].apelido);
                     time['atletas'][i].slug = undefined;
-                    time['atletas'][i].foto = undefined;
-                    time['atletas'][i].clube_id = undefined;
+                    // time['atletas'][i].foto = undefined;
+                    // time['atletas'][i].clube_id = undefined;
                     time['atletas'][i].preco_num = undefined;
                     time['atletas'][i].variacao_num = undefined;
                     time['atletas'][i].jogos_num = undefined;
@@ -64,7 +64,7 @@ module.exports = {
                     time['atletas'][i].posicao_classe = `pos-${posicao}`;
                     time['atletas'][i].capitao = time['capitao_id'] === time['atletas'][i].atleta_id ? true : false;
                 }
-                time['clubes'] = undefined;
+                // time['clubes'] = undefined;
                 time['posicoes'] = undefined;
                 time['status'] = undefined;
                 time['capitao_id'] = undefined;
